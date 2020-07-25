7d57499f75de7bc0742304983cc375f2b9d4547f
denishm116 <denishm116@yandex.ru>
2020-07-25 15:43:28 +0300

    first

---
+++ components/auth/Social.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="center">
+      <v-btn class="ma-2" color="indigo" dark>
+        <fa icon="cog" spin/>
+      </v-btn>
+
+      <v-btn class="ma-2" color="indigo" prevent dark @click="socialLogin('google')">
+        <fa :icon="['fab', 'google']" />
+      </v-btn>
+
+      <v-btn class="ma-2" color="indigo" dark @click="socialLogin('yandex')">
+        <fa :icon="['fab', 'yandex']" />
+      </v-btn>
+
+      <v-btn class="ma-2" color="indigo" dark @click="socialLogin('facebook')">
+        <fa :icon="['fab', 'facebook-f']" />
+      </v-btn>
+
+      <v-btn class="ma-2" color="indigo" dark @click="socialLogin('vkontakte')">
+        <fa :icon="['fab', 'vk']" />
+      </v-btn>
+
+   </div>
+</template>
+
+<script>
+
+    export default {
+methods: {
+  socialLogin(service) {
+    window.location.href = `${process.env.baseUrl}auth/login/${service}`;
+  }
+}
+
+
+
+    }
+</script>
+
+<style scoped>
+.center {
+  margin: 0 auto;
+}
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>HOME</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">HOME</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>HOME</div>
+      {{$auth.$state.loggedIn}}
+    </v-card-text>
+
+    <v-card-actions>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        HOME
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        HOME
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<script>
+  export default {
+    mounted() {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
